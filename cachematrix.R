0baeb7099762bbbde6d90dd9f092c99ae86928cd
alhidalg <aahidalg@cr.ibm.com>
2016-10-09 18:18:21 -0600

    cambios de codigo

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,23 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## basically we create a matrix that self cache in inversa
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+    volta <- NULL
+    set <- function(y) {
+      x <<- y
+      volta <<- NULL
+    }
+    get <- function() x
+    setinversa <- function(inversa) volta <<- inversa
+    getinversa <- function() volta
+    list(set = set,
+         get = get,
+         setinversa = setinversa,
+         getinversa = getinversa)
+  
 
 }
 
@@ -11,5 +25,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inversa of 'x'
+  
+  volta <- x$getinversa()
+  if (!is.null(volta)) {
+    message("Obteniendo Cache...")
+    return(volta)
+  }
+  mat <- x$get()
+  volta <- solve(mat, ...)
+  x$setinversa(volta)
+  volta
+  
 }
